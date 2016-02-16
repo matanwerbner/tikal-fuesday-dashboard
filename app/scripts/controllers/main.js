45622fefbd7e36ef614cca0d6c22baf29bd31130
arturdnt <arturdnt@gmail.com>
2016-02-16 16:39:30 +0200

    blah

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,22 +10,41 @@
 angular.module('workersMapApp')
 .controller('MainCtrl', ['$scope', 'DataService', function($scope, DataService) {
 
-    var json =[{
-        name: 'user1',
-        count: 2
-    },{
-        name: 'user2',
-        count:3
-    }];
 
+
+    $scope.usersLabels =[];
+    $scope.channels =[];
+     $scope.usersScores= [];
+    
     DataService.loadData().then(function(data){
-      debugger;
+    //  debugger;
+    
+        $scope.usersLabels = data.data.users.map(function(d){
+            return d.name;
+        })
+        
+        $scope.usersScores = [data.data.users.map(function(d){
+            return parseInt(d.count);
+        })];
+        
+        $scope.channelsLabels = data.data.channels.map(function(d){
+            return d.name;
+        })
+        
+        $scope.channelScores = [data.data.channels.map(function(d){
+            return parseInt(d.count);
+        })];
+        
+        //debugger;
     });
-     $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-
-
-  $scope.data = [
-    [28, 48, 40, 19, 42, 27, 50]
-  ];
+     //$scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+/*
+$scope.colours =  [{
+    fillColor: 'rgba(47, 132, 71, 0.8)',
+    strokeColor: 'rgba(47, 132, 71, 0.8)',
+    highlightFill: 'rgba(47, 132, 71, 0.8)',
+    highlightStroke: 'rgba(47, 132, 71, 0.8)'
+}];*/
+  
 
   }]);
