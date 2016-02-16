da9bedc0b9a13148b5a36a8d6995ef3be5e40bca
arturdnt <arturdnt@gmail.com>
2016-02-16 16:56:59 +0200

    final for now

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -28,13 +28,21 @@ angular.module('workersMapApp')
       })]
     });
     //$scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    /*
-    $scope.colours =  [{
+    
+       $scope.coloursUsers =  [{
+        "fillColor": "rgba(224, 108, 112, 1)",
+          "strokeColor": "rgba(207,100,103,1)",
+          "pointColor": "rgba(220,220,220,1)",
+          "pointStrokeColor": "#fff",
+          "pointHighlightFill": "#fff",
+          "pointHighlightStroke": "rgba(151,187,205,0.8)"
+    }];
+    $scope.coloursChannels =  [{
         fillColor: 'rgba(47, 132, 71, 0.8)',
         strokeColor: 'rgba(47, 132, 71, 0.8)',
         highlightFill: 'rgba(47, 132, 71, 0.8)',
         highlightStroke: 'rgba(47, 132, 71, 0.8)'
-    }];*/
+    }];
 
 
   }]);
